41b43c951aac6d2bd8a02a5d9d83eb1aec936b76
chuahziyang <czyang2002@gmail.com>
2021-08-17 11:59:14 +0800

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+  <v-app>
+    <v-main>
+      <HelloWorld />
+    </v-main>
+  </v-app>
 </template>
 
 <script lang="ts">
@@ -11,19 +12,13 @@ import HelloWorld from "./components/HelloWorld.vue";
 
 export default Vue.extend({
   name: "App",
+
   components: {
     HelloWorld,
   },
+
+  data: () => ({
+    //
+  }),
 });
 </script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
