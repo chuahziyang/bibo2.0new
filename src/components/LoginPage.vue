e29241145999bf778878bdfa0f6ec53adfcf4a97
chuahziyang <czyang2002@gmail.com>
2021-08-17 16:47:28 +0800

    finish front end for login page, doing router next

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -1,22 +1,22 @@
 <template>
    <v-app>
       <Header />
-      <v-content>
+      <v-main>
          <v-container fluid fill-height>
             <v-layout align-center justify-center>
                <v-flex xs12 sm8 md4>
-                  <v-card class="elevation-12">
+                  <v-card v-if="!this.showRegister" class="elevation-12">
                      <v-toolbar dark color="primary">
                         <v-toolbar-title>Bibo-App</v-toolbar-title>
                      </v-toolbar>
                      <v-card-text>
                         <v-form >
-                           <v-text-field
+                           <v-text-field v-model="user"
                               name="login"
                               label="Login"
                               type="text"
                            ></v-text-field>
-                           <v-text-field
+                           <v-text-field v-model="pass"
                               id="password"
                               name="password"
                               label="Password"
@@ -26,13 +26,55 @@
                      </v-card-text>
                      <v-card-actions>
                         <v-spacer></v-spacer>
-                        <v-btn color="primary" to="/">Login</v-btn>
+                        <v-btn @click="login()" color="primary">Login</v-btn>
+                        <v-btn @click="register()" color="primary">Register</v-btn>
                      </v-card-actions>
                   </v-card>
+                  <!-- REGISTER COMPONENTS -->
+                  <v-card v-if="this.showRegister" class="elevation-12">
+                     <v-toolbar dark color="primary">
+                        <v-toolbar-title>Register</v-toolbar-title>
+                     </v-toolbar>
+                     <v-card-text>
+                        <v-form >
+                           <v-text-field v-model="persid"
+                              name="PERSID"
+                              label="PERSID"
+                              type="text"
+                           ></v-text-field>
+                           <v-text-field v-model="registerpass"
+                              id="password"
+                              name="password"
+                              label="Password"
+                              type="password"
+                           ></v-text-field>
+                           <v-text-field v-model="confirmpass"
+                              id="Confirm Password"
+                              name="Confirm Password"
+                              label="Confirm Password"
+                              type="password"
+                           ></v-text-field>
+                           <v-text-field v-model="authpass"
+                              id="authpass"
+                              name="authpass"
+                              label="Authentication Password"
+                              type="password"
+                           ></v-text-field>
+                        </v-form>
+                     </v-card-text>
+                     <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn v-if="!this.showRegister" @click="login()" color="primary">Login</v-btn>
+                        <v-btn v-if="!this.showRegister" @click="register()" color="primary">Register</v-btn>
+                        <v-btn v-if="this.showRegister" @click="registeruser()" color="primary">Submit</v-btn>
+                     </v-card-actions>
+                  </v-card>
+
+
                </v-flex>
             </v-layout>
          </v-container>
-      </v-content>
+      </v-main>
    </v-app>
 </template>
 
@@ -42,5 +84,20 @@ import Header from "./Header.vue";
 export default Vue.extend({
   components: { Header },
   name: "LoginPage",
+  data(){
+    return{
+      user:"",
+      pass:"",
+      showRegister:false
+    }
+  },
+  methods:{
+    send(){
+      console.log(this.user+"\n"+this.pass)
+    },
+    register(){
+       this.showRegister = true
+    }
+  }
 });
 </script>
